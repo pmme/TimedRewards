50b79067cf40a2fc7335e88ce7c96dcf2377ece3
ImABradley <imabradley@null.spigotmc.org>
2016-12-06 21:24:26 +0000

    version v0.1

--- TimedRewards/src/imabradley/timedrewards/menus/RewardsMenu.java
+++ TimedRewards/src/imabradley/timedrewards/menus/RewardsMenu.java
@@ -1,7 +1,28 @@
 package imabradley.timedrewards.menus;
 
-public class RewardsMenu
+import imabradley.timedrewards.util.abstracts.Menu;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class RewardsMenu extends Menu
 {
 
 
+	public RewardsMenu(String title, int size)
+	{
+		super(title, size);
+	}
+
+	@Override
+	public void click(Player player, ItemStack itemStack)
+	{
+
+	}
+
+	@Override
+	public Inventory getInventory()
+	{
+		return null;
+	}
 }
