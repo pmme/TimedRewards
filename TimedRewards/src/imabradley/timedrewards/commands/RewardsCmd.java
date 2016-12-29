2f12715715b42a6e584b63fa6ca9e55fadbe4b97
Bradley <bradleysteele200@gmail.com>
2016-12-29 16:28:58 +0000

    Initial commit.

--- TimedRewards/src/imabradley/timedrewards/commands/RewardsCmd.java
+++ TimedRewards/src/imabradley/timedrewards/commands/RewardsCmd.java
@@ -48,8 +48,8 @@ public class RewardsCmd implements CommandExecutor
 						{
 							Util.messagePlayer(player, TimedRewards.getYamlHandler()
 									.getMessage("invalid-arguments")
-									.replace("{prefix}", TimedRewards.getYamlHandler().getPrefix())
-									.replace("{args}", "/tr claim <reward-name>"));
+									.replaceAll("(?i)\\{prefix}", TimedRewards.getYamlHandler().getPrefix())
+									.replaceAll("(?i)\\{args}", "/tr claim <reward-name>"));
 						}
 					}
 				}
@@ -57,8 +57,8 @@ public class RewardsCmd implements CommandExecutor
 				{
 					Util.messagePlayer(player, TimedRewards.getYamlHandler()
 							.getMessage("invalid-arguments")
-							.replace("{PREFIX}", TimedRewards.getYamlHandler().getPrefix())
-							.replace("{ARGS}", "/tr help"));
+							.replaceAll("(?i)\\{prefix}", TimedRewards.getYamlHandler().getPrefix())
+							.replaceAll("(?i)\\{args}", "/tr help"));
 				}
 			}
 		}
