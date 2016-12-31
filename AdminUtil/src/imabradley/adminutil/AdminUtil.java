20b19a693b6c9d7c9672f3dbbc2ebac3639c07d9
Bradley <bradleysteele200@gmail.com>
2016-12-31 14:38:41 +0000

    Initial commit.

---
+++ AdminUtil/src/imabradley/adminutil/AdminUtil.java
@@ -0,0 +1,20 @@
+package imabradley.adminutil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AdminUtil extends JavaPlugin
+{
+
+	private AdminUtil plugin;
+
+	@Override public void onEnable()
+	{
+		plugin = this;
+
+	}
+
+	@Override public void onDisable()
+	{
+
+	}
+}
